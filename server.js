cbe9f65f25d974f3291001a3dee037178cabe91f
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2023-11-10 11:46:23 +0700

    доработать загрузку картинок

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const pg = require('./db');
-const fs = require("fs");
 const path = require("path");
 const fileUpload = require("express-fileupload");
-const { randomUUID } = require("crypto");
 const cors = require("cors");
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(fileUpload({}));
-app.use(cors());
 
 
 app.get('/api/news', async (req, res) => {
@@ -27,35 +26,33 @@ app.get("/api/news/:id", async (req, res) => {
 
 app.post('/api/news', async (req, res) => {
     const {title, text} = req.body;
-    const uploadedFile = req.files.uploadFile; 
-    if(uploadedFile === undefined) {
-        res.send("Пустой файл");
-    }
+    console.log(req.body);      
+    const uploadedFile = req.files.uploadFile;
+
     //let fileName = randomUUID() + ".jpg";
     //img.mv(path.resolve(__dirname, "..", "public", fileName));
     if(title.length > 120) {
-        res.end("Больше 120 символов - ограничение БД");
-    } else {
-
-        if (req.files && Object.keys(req.files).length !== 0) { 
+        return res.end("Больше 120 символов - ограничение БД");
+    } 
+       if (req.files && Object.keys(req.files).length !== 0) { 
+            console.log(uploadedFile);
             const uploadPath = __dirname 
                 + "/public/" + uploadedFile.name; 
           
             uploadedFile.mv(uploadPath, function (err) { 
               if (err) { 
                 console.log(err); 
-                res.send("Failed !!"); 
-              } else res.send("Successfully Uploaded !!"); 
+                return res.send("Failed !!"); 
+              } else {
+                return res.send("Successfully Uploaded !!");
+              } 
             }); 
-        } else res.send("No file uploaded !!"); 
-        const addNews = await pg.query(`INSERT INTO news (img, title, text) VALUES ($1, $2, $3)`, [uploadedFile.name, title, text], (err, result) => {
-            if(err) {
-                res.send(err);
-            } else {
-            res.status(201).end(JSON.stringify(addNews));
-            }
-        });
-    }
+        } else {
+            return res.send("No file uploaded !!");
+        };
+        
+        const addNews = await pg.query(`INSERT INTO news (img, title, text) VALUES ($1, $2, $3)`, [uploadedFile.name, title, text]);
+        return res.status(201).json(addNews.rows[0]);
 });
 
 app.delete("/api/news/:id", async (req, res) => {
@@ -64,22 +61,17 @@ app.delete("/api/news/:id", async (req, res) => {
         res.end("Новости с такой ID нет");
     } else {
         const deleteNews = await pg.query(`DELETE FROM news WHERE id = $1`, [id]);
-        res.status(200).end(JSON.stringify(deleteNews));    
+        return res.status(200).end(JSON.stringify(deleteNews));    
     }
 });
 
-app.post("/file", (req, res) => {
-    const file = req.files.file;
-    res.end(console.log(file))
-})
-
 app.put('/api/news/:id', async (req, res) => {
     const id = req.params.id;
-    const {img, title, text} = req.body;
+    const {title, text} = req.body;
     if(title.length > 120) {
-        res.end("Введено больше 120 символов");
+        return res.end("Введено больше 120 символов");
     } else {
-        const uploadedFile = req.files.uploadFile; 
+        let uploadedFile = req.files.uploadFile; 
 
         if (req.files && Object.keys(req.files).length !== 0) { 
             const uploadPath = __dirname 
@@ -88,7 +80,7 @@ app.put('/api/news/:id', async (req, res) => {
             uploadedFile.mv(uploadPath, function (err) { 
               if (err) { 
                 console.log(err); 
-                res.send("Failed !!"); 
+                return res.send("Failed !!"); 
               } else res.send("Successfully Uploaded !!"); 
             }); 
         } else res.send("No file uploaded !!"); 
@@ -96,8 +88,19 @@ app.put('/api/news/:id', async (req, res) => {
             if(err) {
                 throw new Error;
             }
-            res.end(JSON.stringify(updateNews));
+            return res.end(JSON.stringify(updateNews));
         });            
+    }
+});
+
+app.post("/login", (req, res) => {
+    const PASSWORD = "nbvcxz";
+    const {password} = req.body;
+    console.log({password});
+    if(password !== PASSWORD || password === "") {
+        return res.status(400).send("Не правильный пароль");
+    } else {
+        return res.send("Вход в админку осуществлен");
     }
 });
 
